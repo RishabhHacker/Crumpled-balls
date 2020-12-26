0781bcdc7cd53a38d135045ccfeab26f2d3c90c1
RishabhHacker <73529254+RishabhHacker@users.noreply.github.com>
2020-12-26 15:55:56 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,49 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+rectMode(CENTER) 
+
+	engine = Engine.create();
+	world = engine.world;
+	Engine.run(engine)
+
+	//Create the Bodies Here.
+dustbin=new Dustbin(720,390,100,10)
+paper=new Paper(100,300,10)
+ground= Bodies.rectangle(width/2,400,width,10,
+	{
+		isStatic:true
+			});
+			World.add(world,ground);
+
+	
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  dustbin.display();
+  paper.display();
+  drawSprites();
+ 
+}
+function keyPressed(){
+	if(keyCode === UP_ARROW){
+		Matter.Body.applyForce(paperObject.body,paperObject.body.position,{x:85,y:85});
+		}
+}
+
+
+
